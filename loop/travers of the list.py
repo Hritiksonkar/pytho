41fb86b75f390d226b5e4272f5ecc60efd4a641d
Hritiksonkar <hritiksonkar355@gmail.com>
2026-03-17 00:30:07 +0530

    Add traversal examples for lists with ascending and descending order

---
+++ loop/travers of the list.py
@@ -0,0 +1,11 @@
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+i=0
+while i < len(nums):
+    print(nums[i])
+    i+=1
+    
+nums = ["iranman", "spiderman", "superman", "batman", "wonderwoman"]
+j=4
+while j >= 0:
+    print(nums[j])
+    j-=1
