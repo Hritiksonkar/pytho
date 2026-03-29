0c3daae809748100444eaadb6322229a94985d6c
Hritiksonkar <hritiksonkar355@gmail.com>
2026-03-29 22:21:39 +0530

    feat: Add scripts for calculating the product of numbers up to 5 and reversing words in a sentence

---
+++ loop/while/calculate num.py
@@ -0,0 +1,10 @@
+num=int(input("Enter a number: "))
+
+product=1
+
+while num<=5:
+    product*=num
+    num+=1
+    
+
+print("The product of all numbers from 1 to the entered number is:", product)
---
+++ loop/while/reversd string.py
@@ -0,0 +1,9 @@
+sentence = input("Enter a sentence: ")
+words=sentence.split()
+
+for word in words:
+    i=len(word)-1
+    while i>=0:
+        print(word[i],end="")
+        i-=1
+    print(" ",end="")
