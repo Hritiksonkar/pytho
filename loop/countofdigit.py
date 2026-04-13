6c90bb7f39bf8080a1c94420d80f8c71eb4361a5
Hritiksonkar <hritiksonkar355@gmail.com>
2026-04-13 23:21:02 +0530

    feat: add scripts to count digits and extract digits from integers using loops

---
+++ loop/countofdigit.py
@@ -0,0 +1,10 @@
+n=576812
+num=n
+count=0
+while num>0:
+    count+=1
+    num//=10
+    
+print("The number of digits in", n, "is", count)
+    
+    
---
+++ loop/extractdigit.py
@@ -0,0 +1,7 @@
+num=5872
+sum=0
+while num>0:
+    rem=num%10
+    print("Remainder:", rem)
+    num=num//10
+print(" sum of digits:", sum)
